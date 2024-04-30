5b0ff3844c682c5081f68657b09fbf02ef23fa13
Jurden <daniele.juradoc@uqvirtual.edu.co>
2024-04-29 23:15:42 -0500

    Desarrollo de clase persona
    estos son los cambio que me permiten imprimir el objeto

--- src/main/java/co/edu/uniquindio/co/App.java
+++ src/main/java/co/edu/uniquindio/co/App.java
@@ -7,6 +7,20 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("----------------------------------------");
         System.out.println( "Hello World!" );
+        System.out.println("----------------------------------------");
+
+        Persona Persona1 = new Persona();
+        
+        Persona1.setNombre("Daniel");
+        Persona1.setApellido("Jurado");
+        Persona1.setDirección("barrio clementina");
+        Persona1.setEdad((byte)18);
+        Persona1.setIdentificacíon(1011092637);
+        Persona1.setTelefono("3127535358");
+
+        System.out.println(Persona1);
+        System.out.println("----------------------------------------");
     }
 }
